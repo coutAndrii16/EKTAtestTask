dd40ec01a4077dc04457fb1f916f46070d6eb0c0
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-04-12 16:03:33 +0300

    added models

---
+++ Models/ModuleModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EKTAtestTask.Models;
+
+public class ModuleModel
+{
+    public int CabinetRow { get; init; }
+    public int CabinetCol { get; init; }
+    public int ModuleRow { get; init; }
+    public int ModuleCol { get; init; }
+    public double Temperature { get; set; }
+    
+    public string DisplayId => $"Каб [{CabinetCol},{CabinetRow}] Мод [{ModuleCol}, {ModuleRow}]";
+}
