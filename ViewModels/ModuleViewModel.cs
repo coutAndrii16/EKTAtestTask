b5a8c717b9045e3fa87482bbd419102fbc725ad6
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-04-13 00:08:39 +0300

    all works

---
+++ ViewModels/ModuleViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using EKTAtestTask.Models;
+using System.Windows.Media;
+
+namespace EKTAtestTask.ViewModels;
+
+public partial class ModuleViewModel : ObservableObject
+{
+    public ModuleModel Model { get; }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(BackgroundColor))]
+    private double _temperature;
+
+    public ModuleViewModel(ModuleModel model)
+    {
+        Model = model;
+        _temperature = model.Temperature;
+    }
+
+    public void RefreshTemperature()
+    {
+        Temperature = Model.Temperature;
+    }
+
+    public SolidColorBrush BackgroundColor => Temperature switch
+    {
+        < 30  => new SolidColorBrush(Color.FromRgb(70, 130, 230)),   // синій
+        < 55  => new SolidColorBrush(Color.FromRgb(60, 200, 100)),   // зелений
+        < 75  => new SolidColorBrush(Color.FromRgb(255, 180, 40)),   // жовтий
+        _     => new SolidColorBrush(Color.FromRgb(220, 50, 50)),    // червоний
+    };
+
+    public string TooltipText =>
+        $"{Model.DisplayId}\nТемпература: {Temperature:F1} °C";
+}
