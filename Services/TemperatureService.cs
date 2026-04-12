f0b4dfc6b124f9ca835ebfdfde991fe94aada6b9
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-04-12 16:23:54 +0300

    added TemperatureService.cs

---
+++ Services/TemperatureService.cs
@@ -0,0 +1,17 @@
+﻿using EKTAtestTask.Models;
+
+namespace EKTAtestTask.Services;
+
+public class TemperatureService
+{
+    private static readonly Random Random = new();
+    public static double Generate() => Math.Round(15 + Random.NextDouble() * 90 + 10, 1);
+    
+    public static void Randomize (IEnumerable<ModuleModel> modules)
+    {
+        foreach (var module in modules)
+        {
+            module.Temperature = Generate();
+        }
+    }
+}
